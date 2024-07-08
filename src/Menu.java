0660de66fa0e78bc3ab6caf453ddb41bd143bbad
Kiennguyen2406 <nguyenduckien2406@gmail.com>
2024-07-08 10:22:32 +0700

    xong menu dau

---
+++ src/Menu.java
@@ -0,0 +1,9 @@
+public class Menu {
+    public void menuSelect() {
+        System.out.println("Chọn chức năng theo số (tiếp tục) :");
+        System.out.println("1,Thêm mới cán bộ.");
+        System.out.println("2,Tìm kiếm theo họ tên.");
+        System.out.println("3,Hiển thị thông tin về danh sách các cán bộ.");
+        System.out.println("4,Thoát khỏi chương trình.");
+    }
+}
